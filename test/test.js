e5f55b01e23460d172e5960097b5b51a4486a0ad
lucasdemassy <lucasdemassy@live.fr>
2020-02-26 19:39:07 +0100

    Create false test

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const expect = require("chai").expect;
 
 describe("Test", () => {
   it("addition", () => {
-    assert.equal(3+8, 11);
+    assert.equal(3+9, 11);
   });
 /*
   describe("DB", () => {
